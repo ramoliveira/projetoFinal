cf09b505342ce1f9634b2351fb4baf2b4f2fa217
lhcosta <39890572+lhcosta@users.noreply.github.com>
2018-06-04 14:39:12 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /*Síntese - Projeto Final - Lucas Henrique - UC17201125 - Ramón Dias - UC
-Objetivo:
-Entrada:
-Saída:
+Objetivo: gestão dos testes do Campeonato de Moto GP, auxiliando o usuário com diversas opções de menu
+Entrada: piloto, equipe, circuito, melhor volta
+Saída: de acordo com o menu de opções
 */
 
 #include <stdio.h>
