1cec0b96390169d417208de6dd5b11e1a51403f0
lhcosta <39890572+lhcosta@users.noreply.github.com>
2018-06-04 13:50:01 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,19 @@
+/*Síntese - Projeto Final - Lucas Henrique - UC17201125 - Ramón Dias - UC
+Objetivo:
+Entrada:
+Saída:
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
-	printf("Ola mundo!");	
-	printf("Projeto Final\n");
-	printf("Deu certo\n");
-
+	
+	/*Declarações*/
+	
+	
+	/*Instruções*/
+	
+	
 	return 0;
 }
